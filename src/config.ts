ff981030ad1971a6dff822857be7bb574f0fb82f
Ranran <sign123in456@hotmail.com>
2026-05-04 19:30:07 +0900

    update footbar

--- src/config.ts
+++ src/config.ts
@@ -418,7 +418,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 
 export const footerConfig: FooterConfig = {
 	enable: true, // 是否启用Footer HTML注入功能
-	customHtml: "<p>资源仅供个人学习使用！", // HTML格式的自定义页脚信息，例如备案号等，默认留空
+	customHtml: "ようこそ<ruby>書庫<rt>ミーミルノヰツヅミ</ruby>・デ・永雏多氢菲へ", // HTML格式的自定义页脚信息，例如备案号等，默认留空
 	// 也可以直接编辑 FooterConfig.html 文件来添加备案号等自定义内容
 	// 注意：若 customHtml 不为空，则使用 customHtml 中的内容；若 customHtml 留空，则使用 FooterConfig.html 文件中的内容
 	// FooterConfig.html 可能会在未来的某个版本弃用
