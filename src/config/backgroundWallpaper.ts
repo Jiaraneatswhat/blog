8bcfaf714142137ce38fd97200b8b00ab5b08333
ranran <ranran@MBA-de-ranran.local>
2026-09-16 10:50:16 +0800

    c

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -67,7 +67,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否允许用户通过控制面板切换横幅标题显示
 			switchable: false,
 			// 主页横幅主标题
-			title: `永雏多氢菲の書庫`,
+            title: `永雏塔菲胶の書庫`,
 			// 主页横幅主标题字体大小
 			titleSize: "3.8rem",
 			// 主页横幅副标题
